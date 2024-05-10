a81dd8e2229496340ca2508bc5453693e6e1364f
AshkanPoyber <AcerMRD.Pro@gmail.com>
2024-05-10 18:06:34 +0330

    updated

--- PaintApp.js
+++ PaintApp.js
@@ -50,6 +50,7 @@ const drawTriangle = (e) => {
   ctx.beginPath(); //Creating New Path To Draw Circle !
   ctx.moveTo(prevMouseX, prevMouseY); //Moving Triangle To The Mouse Pointer !
   ctx.lineTo(e.offsetX, e.offsetY); //Creating First Line According To The Mouse Pointer !
+  ctx.stroke();
 };
 
 const startDraw = (e) => {
