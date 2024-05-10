217414c8ad1769ffc38438ed8ad578837a7f1c3f
AshkanPoyber <AcerMRD.Pro@gmail.com>
2024-05-10 10:51:09 +0330

    updated

--- PaintApp.js
+++ PaintApp.js
@@ -1,8 +1,9 @@
 const canvas = document.guerySelector("canvas"),
   ctx = canvas.getContext("2d");
 
-const drawing = () => {
-  ctx.LineTo();
+const drawing = (e) => {
+  ctx.LineTo(e.offsetX, e.offsetY); //Creating Line According To The Mouse Pointer !
+  ctx.stroke(); //Drawing/Filling Line With Color !
 };
 
 canvas.addEventlistener("mousemove", drawing);
