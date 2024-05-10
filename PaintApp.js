51579bbd26d2474b3eb4681506910b20ffbd8c58
AshkanPoyber <AcerMRD.Pro@gmail.com>
2024-05-10 11:11:36 +0330

    updated

--- PaintApp.js
+++ PaintApp.js
@@ -1,6 +1,8 @@
 const canvas = document.querySelector("canvas"),
   ctx = canvas.getContext("2d");
 
+let isDrawing = false;
+
 window.addEventListener("load", () => {
   //Setting Canvas Width & Height ~ offsetwidth & height Return Viewable Width & Height Of An Element !
   canvas.width = canvas.offsetWidth;
@@ -8,6 +10,7 @@ window.addEventListener("load", () => {
 });
 
 const drawing = (e) => {
+  if (!isDrawing) return; //If isdrawing Is False return From Here !
   ctx.lineTo(e.offsetX, e.offsetY); //Creating Line According To The Mouse Pointer !
   ctx.stroke(); //Drawing & Filling Line With Color !
 };
