465fac5f4ce488661b9e137f86ac32eccb0dffc4
AshkanPoyber <AcerMRD.Pro@gmail.com>
2024-05-11 22:43:54 +0330

    updated

--- PaintApp.js
+++ PaintApp.js
@@ -2,7 +2,7 @@ const canvas = document.querySelector("canvas"),
   toolBtns = document.querySelectorAll(".tool"),
   fillColor = document.querySelector("#fill-color"),
   sizeSlider = document.querySelector("#size-slider"),
-  colorBtns = document.querySelector(".colors .option"),
+  colorBtns = document.querySelectorAll(".colors .option"),
   ctx = canvas.getContext("2d");
 
 //Global Variables With Default Value !
